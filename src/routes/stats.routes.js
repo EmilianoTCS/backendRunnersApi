f66789a7df6f03a549014245abeb12402a4c26cc
EmilianoTCS <96253955+EmilianoTCS@users.noreply.github.com>
2024-11-17 22:09:53 -0300

    added historicStats
    hola

--- src/routes/stats.routes.js
+++ src/routes/stats.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { documentCount } from "../controllers/stats.controllers";
+import { documentCount, HistoricStats } from "../controllers/stats.controllers";
 import { verifyToken } from "../middlewares/verifyToken";
 
 const router = Router();
 
 router.get("/getStats", [verifyToken], documentCount);
+router.get("/getHistoricStats", [verifyToken], HistoricStats);
 export default router;
