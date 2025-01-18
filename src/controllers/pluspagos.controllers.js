2534064a144976a1bab2502db8069f1e8b51a4ce
EmilianoTCS <96253955+EmilianoTCS@users.noreply.github.com>
2025-01-18 18:46:20 -0300

    añadido de post pagos exitosos

--- src/controllers/pluspagos.controllers.js
+++ src/controllers/pluspagos.controllers.js
@@ -31,3 +31,20 @@ export const payProduct = async (req, res) => {
     res.status(500).send("Error encrypting data");
   }
 };
+
+//Función que se ejecuta después de realizar el pago del link generado anteriormente, recibe el body enviado por el script de MP y el ID de la cuota.
+export const SuccessfullPayment = async (req, res) => {
+  const { feeID } = req.body;
+  try {
+    //Establezco los filtros y los parámetros a actualizar
+    const filterActual = { _id: feeID };
+    const updateActual = { isActive: false, isPayed: true };
+
+    await Fee.findOneAndUpdate(filterActual, updateActual, { new: true });
+
+    res.status(200).json({ message: "Operación exitosa." });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+};
--- src/routes/plusplagos.routes.js
+++ src/routes/plusplagos.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/verifyToken";
-import { payProduct } from "../controllers/pluspagos.controllers";
+import {
+  payProduct,
+  SuccessfullPayment,
+} from "../controllers/pluspagos.controllers";
 const router = Router();
 
 router.post("/encryptData", [verifyToken], payProduct);
+router.post("/successfullPayment", [verifyToken], SuccessfullPayment);
 
 export default router;
